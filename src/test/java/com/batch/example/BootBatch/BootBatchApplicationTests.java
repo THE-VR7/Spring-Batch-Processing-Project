63ac65f9558bebd6bc81ea0ed533a62130cabff6
vineet rathor <vineetrathor57@gmail.com>
2025-08-15 23:36:26 +0530

    Initial Commit for Spring Batch Processing Project

---
+++ src/test/java/com/batch/example/BootBatch/BootBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batch.example.BootBatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
